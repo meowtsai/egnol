be8be013e0204dd93638c97ad7bdfaaccb7b0081
oldking0910 <oldking.gary@gmail.com>
2015-06-30 18:19:14 +0800

    Modify css for mobile browser

--- default/application/views/platform/index.php
+++ default/application/views/platform/index.php
@@ -1,9 +1,9 @@
-<div style="width:90%;max-width:640px;margin:0 auto;text-align:center;">
-	<div style="border:2px solid #ddd;width:100%;height:240px;"></div>
-	<div style="padding:10px 0;width:100%;margin:0 auto;">
-		<div style="display:inline-block;width:30%;height:200px;border:2px solid #777;">QR Code
-		</div><div style="display:inline-block;width:30%;height:200px;border:2px solid #777;">儲值
-		</div><div style="display:inline-block;width:30%;height:200px;border:2px solid #777;">下載</div>
+<div style="width:90%;max-width:640px;height:100%;min-height:320px;margin:0 auto;text-align:center;">
+	<div style="border:2px solid #ddd;width:100%;height:30%;"></div>
+	<div style="padding:10px 0;width:100%;height:30%;margin:0 auto;">
+		<div style="display:inline-block;width:30%;height:90%;border:2px solid #777;">QR Code
+		</div><div style="display:inline-block;width:30%;height:90%;border:2px solid #777;">儲值
+		</div><div style="display:inline-block;width:30%;height:90%;border:2px solid #777;">下載</div>
 	</div>
-	<div style="border:2px solid #ddd;width:100%;height:240px;margin:0 auto;"></div>
+	<div style="border:2px solid #ddd;width:100%;height:30%;margin:0 auto;"></div>
 </div>
