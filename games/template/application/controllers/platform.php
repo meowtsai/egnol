86af34a2f7d676ed1fd5348932c3bc5bc7d31264
oldking0910 <oldking.gary@gmail.com>
2015-07-15 13:51:25 +0800

    Revert "Add game template files"
    This reverts commit 6caafda3c17ab981b8b7953f3c2528d6c97013eb.

--- games/template/application/controllers/platform.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Platform extends MY_Controller
-{
-	function index()
-	{		
-		$this->_init_layout()->standard_view("index");
-	}
-}
