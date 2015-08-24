0c11e02a862fce398301f45f907232f3733fd04e
oldking0910 <oldking.gary@gmail.com>
2015-08-24 15:23:37 +0800

    New copyright display

--- ci_system/global/views/g_copyright.php
+++ ci_system/global/views/g_copyright.php
@@ -1,5 +1,6 @@
-<div id="le_footer">
-	<div id="copyright">
-	 	龍邑科技 版權所有 2015 longeplay.com All Rights Reserved
-	</div>
+<div style="clear:both;"></div>
+<div class="copyright"><p>Long E Co., Ltd © 2015 Copyrights Reserved.</p></div>
+<footer>
+<div class="footer-ins">
 </div>
+</footer>
