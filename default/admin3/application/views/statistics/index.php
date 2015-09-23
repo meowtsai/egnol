e5f6e778e624d1fb9900fa8ca0742e1c29eb61ce
root <root@localhost.localdomain>
2015-09-23 12:24:58 +0000

    statistics/index retention fix

--- default/admin3/application/views/statistics/index.php
+++ default/admin3/application/views/statistics/index.php
@@ -51,15 +51,15 @@
 			</tr>
 			<tr>
 				<td nowrap="nowrap">次日留存</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->new_login_count_1)?$row0[0]->one_retention_count/$row0[0]->new_login_count_1:"")?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->one_retention_count&&$row0[0]->new_login_count_1&&$row1[0]->new_login_count_1)?100*($row0[0]->one_retention_count/$row0[0]->new_login_count_1)/($row1[0]->one_retention_count/$row1[0]->new_login_count_1):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->one_retention_count&&$row0[0]->new_login_count_1&&$row2[0]->new_login_count_1)?100*($row0[0]->one_retention_count/$row0[0]->new_login_count_1)/($row2[0]->one_retention_count/$row2[0]->new_login_count_1):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->new_login_count_1)?$row0[0]->one_retention_count/$row0[0]->new_login_count_1:0)?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->one_retention_count&&$row0[0]->new_login_count_1&&$row1[0]->new_login_count_1)?100*($row0[0]->one_retention_count/$row0[0]->new_login_count_1)/($row1[0]->one_retention_count/$row1[0]->new_login_count_1):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->one_retention_count&&$row0[0]->new_login_count_1&&$row2[0]->new_login_count_1)?100*($row0[0]->one_retention_count/$row0[0]->new_login_count_1)/($row2[0]->one_retention_count/$row2[0]->new_login_count_1):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">7日留存</td>
 				<td style="text-align:right"><?=number_format(($row0[0]->new_login_count_7)?$row0[0]->seven_retention_count/$row0[0]->new_login_count_7:0)?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->seven_retention_count&&$row0[0]->new_login_count_7&&$row1[0]->new_login_count_7)?100*($row0[0]->seven_retention_count/$row0[0]->new_login_count_7)/($row1[0]->seven_retention_count/$row1[0]->new_login_count_7):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->seven_retention_count&&$row0[0]->new_login_count_7&&$row2[0]->new_login_count_7)?100*($row0[0]->seven_retention_count/$row0[0]->new_login_count_7)/($row2[0]->seven_retention_count/$row2[0]->new_login_count_7):"").'%':""?></td>																
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->seven_retention_count&&$row0[0]->new_login_count_7&&$row1[0]->new_login_count_7)?100*($row0[0]->seven_retention_count/$row0[0]->new_login_count_7)/($row1[0]->seven_retention_count/$row1[0]->new_login_count_7):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->seven_retention_count&&$row0[0]->new_login_count_7&&$row2[0]->new_login_count_7)?100*($row0[0]->seven_retention_count/$row0[0]->new_login_count_7)/($row2[0]->seven_retention_count/$row2[0]->new_login_count_7):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">登入設備</td>
@@ -69,7 +69,7 @@
 			</tr>
 			<tr>
 				<td nowrap="nowrap">平均在線</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?$row0[0]->total_time/$row0[0]->login_count:"")?></td>
+				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?$row0[0]->total_time/$row0[0]->login_count:0)?></td>
 				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->login_count && $row1[0]->total_time && $row1[0]->login_count)?100*($row0[0]->total_time/$row0[0]->login_count)/($row1[0]->total_time/$row1[0]->login_count):0).'%':""?></td>
 				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->login_count && $row2[0]->total_time && $row2[0]->login_count)?100*($row0[0]->total_time/$row0[0]->login_count)/($row2[0]->total_time/$row2[0]->login_count):0).'%':""?></td>																
 			</tr>
@@ -99,27 +99,27 @@
 			</tr>
 			<tr>
 				<td nowrap="nowrap">日付費比率</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?100*$row0[0]->deposit_user_count/$row0[0]->login_count:"")."%"?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->login_count && $row1[0]->deposit_user_count && $row1[0]->login_count)?100*($row0[0]->deposit_user_count/$row0[0]->login_count)/($row1[0]->deposit_user_count/$row1[0]->login_count):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->login_count && $row2[0]->deposit_user_count && $row2[0]->login_count)?100*($row0[0]->deposit_user_count/$row0[0]->login_count)/($row2[0]->deposit_user_count/$row2[0]->login_count):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?100*$row0[0]->deposit_user_count/$row0[0]->login_count:0)."%"?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->login_count && $row1[0]->deposit_user_count && $row1[0]->login_count)?100*($row0[0]->deposit_user_count/$row0[0]->login_count)/($row1[0]->deposit_user_count/$row1[0]->login_count):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->login_count && $row2[0]->deposit_user_count && $row2[0]->login_count)?100*($row0[0]->deposit_user_count/$row0[0]->login_count)/($row2[0]->deposit_user_count/$row2[0]->login_count):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">ARPU</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?100*$row0[0]->deposit_total/$row0[0]->login_count:"")."%"?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->login_count && $row1[0]->deposit_total && $row1[0]->login_count)?100*($row0[0]->deposit_total/$row0[0]->login_count)/($row1[0]->deposit_total/$row1[0]->login_count):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->login_count && $row2[0]->deposit_total && $row2[0]->login_count)?100*($row0[0]->deposit_total/$row0[0]->login_count)/($row2[0]->deposit_total/$row2[0]->login_count):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->login_count)?100*$row0[0]->deposit_total/$row0[0]->login_count:0)."%"?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->login_count && $row1[0]->deposit_total && $row1[0]->login_count)?100*($row0[0]->deposit_total/$row0[0]->login_count)/($row1[0]->deposit_total/$row1[0]->login_count):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->login_count && $row2[0]->deposit_total && $row2[0]->login_count)?100*($row0[0]->deposit_total/$row0[0]->login_count)/($row2[0]->deposit_total/$row2[0]->login_count):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">ARPPU</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->deposit_user_count)?100*$row0[0]->deposit_total/$row0[0]->deposit_user_count:"")."%"?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->deposit_user_count && $row1[0]->deposit_total && $row1[0]->deposit_user_count)?100*($row0[0]->deposit_total/$row0[0]->deposit_user_count)/($row1[0]->deposit_total/$row1[0]->deposit_user_count):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->deposit_user_count && $row2[0]->deposit_total && $row2[0]->deposit_user_count)?100*($row0[0]->deposit_total/$row0[0]->deposit_user_count)/($row2[0]->deposit_total/$row2[0]->deposit_user_count):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->deposit_user_count)?100*$row0[0]->deposit_total/$row0[0]->deposit_user_count:0)."%"?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->deposit_user_count && $row1[0]->deposit_total && $row1[0]->deposit_user_count)?100*($row0[0]->deposit_total/$row0[0]->deposit_user_count)/($row1[0]->deposit_total/$row1[0]->deposit_user_count):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->deposit_user_count && $row2[0]->deposit_total && $row2[0]->deposit_user_count)?100*($row0[0]->deposit_total/$row0[0]->deposit_user_count)/($row2[0]->deposit_total/$row2[0]->deposit_user_count):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">新用戶1日價值</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->new_login_count)?$row0[0]->one_ltv/$row0[0]->new_login_count:"")?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->new_login_count && $row1[0]->one_ltv && $row1[0]->new_login_count)?100*($row0[0]->one_ltv/$row0[0]->new_login_count)/($row1[0]->one_ltv/$row1[0]->new_login_count):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->new_login_count && $row2[0]->one_ltv && $row2[0]->new_login_count)?100*($row0[0]->one_ltv/$row0[0]->new_login_count)/($row2[0]->one_ltv/$row2[0]->new_login_count):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->new_login_count)?$row0[0]->one_ltv/$row0[0]->new_login_count:0)?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->new_login_count && $row1[0]->one_ltv && $row1[0]->new_login_count)?100*($row0[0]->one_ltv/$row0[0]->new_login_count)/($row1[0]->one_ltv/$row1[0]->new_login_count):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->new_login_count && $row2[0]->one_ltv && $row2[0]->new_login_count)?100*($row0[0]->one_ltv/$row0[0]->new_login_count)/($row2[0]->one_ltv/$row2[0]->new_login_count):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">累積安裝數</td>
@@ -130,26 +130,26 @@
 			<tr>
 				<td nowrap="nowrap">累積用戶</td>
 				<td style="text-align:right"><?=number_format($row0[0]->total_users)?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->total_users)?100*$row0[0]->total_users/$row1[0]->total_users:"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->total_users)?100*$row0[0]->total_users/$row2[0]->total_users:"").'%':""?></td>																
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->total_users)?100*$row0[0]->total_users/$row1[0]->total_users:0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->total_users)?100*$row0[0]->total_users/$row2[0]->total_users:0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">累積營收</td>
 				<td style="text-align:right"><?=number_format($row0[0]->historical_revenue_sum)?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->historical_revenue_sum)?100*$row0[0]->historical_revenue_sum/$row1[0]->historical_revenue_sum:"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->historical_revenue_sum)?100*$row0[0]->historical_revenue_sum/$row2[0]->historical_revenue_sum:"").'%':""?></td>																
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->historical_revenue_sum)?100*$row0[0]->historical_revenue_sum/$row1[0]->historical_revenue_sum:0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->historical_revenue_sum)?100*$row0[0]->historical_revenue_sum/$row2[0]->historical_revenue_sum:0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">整體付費率</td>
-				<td style="text-align:right"><?=number_format(($row0[0]->total_users)?100*$row0[0]->historical_deposit_user_count/$row0[0]->total_users:"").'%'?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->total_users && $row1[0]->historical_deposit_user_count && $row1[0]->total_users)?100*($row0[0]->historical_deposit_user_count/$row0[0]->total_users)/($row1[0]->historical_deposit_user_count/$row1[0]->total_users):"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->total_users && $row2[0]->historical_deposit_user_count && $row2[0]->total_users)?100*($row0[0]->historical_deposit_user_count/$row0[0]->total_users)/($row2[0]->historical_deposit_user_count/$row2[0]->total_users):"").'%':""?></td>																
+				<td style="text-align:right"><?=number_format(($row0[0]->total_users)?100*$row0[0]->historical_deposit_user_count/$row0[0]->total_users:0).'%'?></td>
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row0[0]->total_users && $row1[0]->historical_deposit_user_count && $row1[0]->total_users)?100*($row0[0]->historical_deposit_user_count/$row0[0]->total_users)/($row1[0]->historical_deposit_user_count/$row1[0]->total_users):0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row0[0]->total_users && $row2[0]->historical_deposit_user_count && $row2[0]->total_users)?100*($row0[0]->historical_deposit_user_count/$row0[0]->total_users)/($row2[0]->historical_deposit_user_count/$row2[0]->total_users):0).'%':""?></td>																
 			</tr>
 			<tr>
 				<td nowrap="nowrap">總付費用戶</td>
 				<td style="text-align:right"><?=number_format($row0[0]->historical_deposit_user_count)?></td>
-				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->historical_deposit_user_count)?100*$row0[0]->historical_deposit_user_count/$row1[0]->historical_deposit_user_count:"").'%':""?></td>
-				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->historical_deposit_user_count)?100*$row0[0]->historical_deposit_user_count/$row2[0]->historical_deposit_user_count:"").'%':""?></td>																
+				<td style="text-align:right"><?=(isset($row1[0]))?number_format(($row1[0]->historical_deposit_user_count)?100*$row0[0]->historical_deposit_user_count/$row1[0]->historical_deposit_user_count:0).'%':""?></td>
+				<td style="text-align:right"><?=(isset($row2[0]))?number_format(($row2[0]->historical_deposit_user_count)?100*$row0[0]->historical_deposit_user_count/$row2[0]->historical_deposit_user_count:0).'%':""?></td>																
 			</tr>
 		</tbody>
 	</table>
