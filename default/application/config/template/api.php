2d9ea842e2ecff123b3490e54777dcb32e4c827f
oldking0910 <oldking.gary@gmail.com>
2016-03-14 11:44:40 +0800

    Add R2G In-App-Billing products

--- default/application/config/template/api.php
+++ default/application/config/template/api.php
@@ -21,6 +21,8 @@ $config["partner_api"] = array(
 					'GooglePlay' => array(
 						'LicenseKey' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyhQESbfjx7ZLKHjfqPr9dbW50GYNf8rB+RvsKw/oIPe9gc05GZZS+Iayq/EVZujHIsdSQZ5llshHbaD1XzWINzwOtXrfmGIlTyWKZgeWvs2TELZ+fnUW5bFD+sR8UF73UMt8Tt+ROf45fwZ0GIyXhtSK3xWo1CNgHy0++Bj8FehULdDLrJVn2u3rey0v9Yy4cg7Az2SsodtQwBVMgI705YCvRx8dATc1EcWtVXOI7YGG74ZLLhsx0WlyNtpWNf1JpV1kmyGNuEBH2XBw0abcEJaEx7t0rzoBG3HG9S8CW3bkFZ47eWPvqrTnAqhOiN/IravB5i7L+eeco0bWad6EZwIDAQAB',
 						'Products' => array(
+							'gold.100',
+							'gold.200'
 						)
 					)
 				),
@@ -54,8 +56,36 @@ $config["partner_api"] = array(
 				'name' => '絕代雙驕',
 				'key' => '85cb5665a2a868410cf740a3c537ab09',
 				'LongE' => array('EnableTracking' => '1'),
-				'Google' => array('Analytics' => array('ID' => 'UA-66673104-2', 'Enable' => '1')),
-				'Apple' => array('ID' => '1014250566'),
+				'Google' => array(
+					'Analytics' => array(
+						'ID' => 'UA-66673104-2',
+						'Enable' => '1'
+					),
+					'GooglePlay' => array(
+						'LicenseKey' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkAFddQDMsmMNdDZXOq1bvgCkmewiKGmPzTEXBDQeGHukQheAzL1z232EykFyubZ6ATzkAjFTPa5HICJ/o+h9h6vkYqob1hKg1x7XslZH3pUpi/jZZel7seN8p3WDOL3W2eFYdna4MSb6nkcJzh2WDdG2k+dvdLoKw8uURHYuRTBv13XDHm8AIkemNPZcVsDdh7Y7S0B5Fl+1WxrnOGzK1XoHvDNhcNPVh/5p8gQhpJLdO53B0EELJ5WgVdQM5F6oKpUS9/G0obIDL3GVwP41E+7QVqA3p1RM91SsGcCpnnN49H6BYe6eDnCNCqSNX3ngppdAD7eo2miC+sdDx4GKmwIDAQAB',
+						'Products' => array(
+							'r2g_60',
+							'r2g_150',
+							'r2g_450',
+							'r2g_590',
+							'r2g_1490',
+							'r2g_2990'
+						)
+					)
+				),
+				'Apple' => array(
+					'ID' => '1014250566',
+					'AppStoreProducts' => array(
+						'longer2g_60',
+						'longer2g_150',
+						'longer2g_450',
+						'longer2g_590',
+						'longer2g_1490',
+						'longer2g_2990',
+						'longer2gmo_120',
+						'longer2gm_240',
+					)
+				),
 				'Facebook' => array('ID' => '152562051774453', 'EventTracking' => '1'),
 				'AppsFlyer' => array('DevKey' => 'RgFZmTS3qoYGsPrfPsaq8c', 'Enable' => '1'),
 			),
