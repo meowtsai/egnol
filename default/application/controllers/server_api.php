ef3c8f003fa47f93f0686d2e9e34ef092a1298dd
oldking0910 <oldking.gary@gmail.com>
2016-04-21 22:28:49 +0800

    Add server-to-server login and create character API

--- default/application/controllers/server_api.php
+++ default/application/controllers/server_api.php
@@ -73,7 +73,91 @@ class Server_api extends MY_Controller
             return false;
         }
     }
+	
+	// 玩家在遊戲 server 中完成遊戲本身的登入程序(在龍邑的登入程序之後)
+	function user_login_complete()
+	{
+		$game_id = $this->input->post("game_id");
+		$uid = $this->input->post("uid");
+		$server_id = $this->input->post("server_id");
+		
+        if(!IN_OFFICE)
+		{
+            die('0');
+        }
+		
+		
+		log_message("error", "user_login_complete:{$uid},{$server_id}");
+		
+		die('1');
+	}
     
+	// 玩家在遊戲中創角色
+	function user_create_character()
+	{
+		$server_id = $this->input->post("server_id");
+		$uid = $this->input->post("uid");
+		$character_id = $this->input->post("character_id");
+		$character_name = $this->input->post("character_name");
+		
+        if(!IN_OFFICE)
+		{
+            die('0');
+        }
+		
+		// 若沒設定 server_id, 則找出最近一次登入的 server
+		if(empty($server_id))
+		{
+			$login_game = $this->db->from("log_game_logins")->where("uid", $uid)->order_by("create_time desc")->limit(1)->get()->row();
+			if(!empty($login_game))
+			{
+				$server_id = $login_game->server_id;
+			}
+		}
+		
+		log_message("error", "user_create_character:{$server_id},{$uid},{$character_id},{$character_name}");
+
+		$server_info = $this->db->from("servers")->where("server_id", $server_id)->get()->row();
+		if (empty($server_info))
+		{
+			$server_info = $this->db->from("servers")->where("address", $server_id)->get()->row();
+			if (empty($server_info))
+			{
+				die('0');
+			}
+			
+			$server_id = $server_info->server_id;
+		}
+		
+		$query = $this->db->from("users")->where("uid", $uid)->get();
+		if ($query->num_rows() == 0)
+		{
+			die('0');
+		}
+		
+		$this->load->model("g_characters");
+		if ($this->g_characters->chk_character_exists($server_info, $uid, $character_name))
+		{
+			die('0');
+		}
+		
+		$insert_id = $this->g_characters->create_character($server_info,
+			array(
+				"uid" => $uid,
+				'name' => $character_name,
+				'in_game_id' => $character_id
+			));
+		
+		if (empty($insert_id))
+		{
+			die('0');
+		}
+
+		log_message("error", "user_create_character:{$insert_id}");
+		
+		die('1');
+	}
+	
     function _return_error($msg) 
     {
     	$this->error_message = $msg;
