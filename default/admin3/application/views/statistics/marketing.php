36980a7a2d33d418ac7c937f292ea57afbde634f
root <root@test-payment.(none)>
2016-04-29 12:07:00 +0800

    Fix marketing statistics broken

--- default/admin3/application/views/statistics/marketing.php
+++ default/admin3/application/views/statistics/marketing.php
@@ -1,114 +1,42 @@
-<?php
-	$channels = $this->config->item('channels'); 
-	$c_game_query = $this->db->from("games")->order_by("rank")->get();
-
-	$c_game = array();
-	$c_game_menu["獨代"] = array();
-	$c_game_menu["聯運"] = array();
-	$c_game_menu["關閉"] = array();
-	
-	$exchange_rate = 1;
-	
-	foreach($c_game_query->result() as $row) {
-		$c_game[$row->game_id] = $row;		
-		if (!$row->is_active) {$c_game_menu["關閉"][] = $row; continue;}
-		if (strpos($row->tags.",", "聯運,") !== false) {$c_game_menu["聯運"][] = $row; continue;}
-		$c_game_menu["獨代"][] = $row;
-		
-		if ($row->game_id == $game_id) $exchange_rate = $row->exchange_rate;
-	}
-?>
 <div id="func_bar">
 	
 </div>
 
-<ul class="nav nav-tabs">
-    <li class="<?=empty($span) ? "active" : ""?>">
-        <a href="<?=site_url("statistics/marketing?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">日報表</a>
+<!--ul class="nav nav-tabs">
+    <li class="<?=(empty($span)) ? "active" : ""?>">
+        <a href="<?=site_url("statistics/operation?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">日報表</a>
     </li>
     <li class="<?=($span=='weekly') ? "active" : ""?>">
-        <a href="<?=site_url("statistics/marketing?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}&span=weekly")?>">週報表</a>
+        <a href="<?=site_url("statistics/operation?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}&span=weekly")?>">週報表</a>
     </li>
     <li class="<?=($span=='monthly') ? "active" : ""?>">
-        <a href="<?=site_url("statistics/marketing?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}&span=monthly")?>">月報表</a>
+        <a href="<?=site_url("statistics/operation?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}&span=monthly")?>">月報表</a>
     </li>
-</ul>
+    <li class="">
+        <a href="<?=site_url("statistics/deposit_level?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">儲值區間分析</a>
+    </li>
+    <li class="">
+        <a href="<?=site_url("statistics/deposit_analysis?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">會員儲值分析</a>
+    </li>
+    <li class="">
+        <a href="<?=site_url("statistics/lifetime_value?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">LTV分析</a>
+    </li>
+    <li class="">
+        <a href="<?=site_url("statistics/game_consumes?game_id={$this->game_id}&start_date={$start_date}&end_date={$end_date}")?>">消費分析</a>
+    </li>
+</ul-->
 
-<? if ($query):?>
-
-<table class="table table-striped table-bordered" style="width:auto;">
-	<thead>
-		<tr>
-			<th style="width:60px;">開始</th>
-			<th style="width:60px;">結束</th>
-			<th style="width:80px">遊戲</th>
-			<th style="width:120px;">活動名稱</th>
-			<th style="width:80px;">累計金額</th>
-			<th style="width:60px;">取消</th>
-			<th style="width:60px;">待匯款</th>
-			<th style="width:60px;">匯款完成</th>
-			<th style="width:60px;">派發完成</th>
-			<th style="width:60px;">結案</th>	
-			<th style="width:60px;">動作</th>	
-		</tr>
-	</thead>
-	<tbody>
-		<? if ($query->num_rows() == 0):?>
-				
-		<tr>
-			<td colspan="10">
-				<div style="padding:10px; color:#777;">查無記錄</div>
-			</td>
-		</tr>
-
-		<? else:?>
-		
-		<? foreach($query->result() as $row):?>
-		<tr>
-			<td><?=date('Y-m-d', strtotime($row->start_date))?></td>
-			<td><?=($row->end_date=='0000-00-00 00:00:00')?"":date('Y-m-d', strtotime($row->end_date))?></td>
-			<td><?=$row->game_name?></td>
-			<td style="word-break: break-all">
-				<a href="<?=site_url("vip/event_view/{$row->id}")?>"><?=mb_strimwidth(strip_tags($row->title), 0, 66, '...', 'utf-8')?></a>
-			</td>
-			<td><?=$row->total?></td>
-			<td><?=$row->cancelled_count?></td>
-			<td><?=$row->pending_count?></td>
-			<td><?=$row->complete_count?></td>
-			<td><?=$row->delivered_count?></td>
-			<td><?=$row->closed_count?></td>
-			<td> </td>
-		</tr>
-		<? endforeach;?>
-		
-		<? endif;?>
-		
-	</tbody>
-</table>
-<? endif;?>
-<form method="get" action="<?=site_url("statistics/marketing")?>" class="form-search">
+<form method="get" action="<?=site_url("statistics/appsflyer")?>" class="form-search">
 	<!--input type="hidden" name="game_id" value="<?=$this->input->get("span")?>"-->
 	<input type="hidden" name="span" value="<?=$this->input->get("span")?>">
 	<div class="control-group">
-		
-		<select name="game_id">
-		    <option value="">--請選擇遊戲--</option>
-			<?
-			foreach($c_game_menu as $category => $c_menu):?>
-				<option value=""> -------- <?=$category?> --------</option>
-				<? foreach($c_menu as $key => $row):?>
-				<option value="<?=$row->game_id?>" <?=($game_id==$row->game_id ? 'selected="selected"' : '')?>><?=$row->name?>	</option>
-				<? endforeach;?>
-			<? $i++;  
-			endforeach;?>	
-	    </select>
 		
 		時間
 		<input type="text" name="start_date" class="date required" value="<?=($this->input->get("start_date"))?$this->input->get("start_date"):date("Y-m-d",strtotime("-8 days"))?>" style="width:120px"> 至
 		<input type="text" name="end_date" class="date" value="<?=$this->input->get("end_date")?>" style="width:120px" placeholder="現在">
 		<a href="javascript:;" class="clear_date"><i class="icon-remove-circle" title="清除"></i></a>
 		
-		<input type="submit" class="btn btn-small btn-inverse" name="action" value="行銷效益數據">	
+		<input type="submit" class="btn btn-small btn-inverse" name="action" value="Appsflyer追蹤">	
 	
 	</div>
 		
@@ -120,73 +48,49 @@
 		<thead>
 			<tr>
 				<th nowrap="nowrap">日期</th>
-				<th style="width:70px">新增用戶</th>
-				<th style="width:70px">登入用戶</th>
-				<th style="width:70px">登入設備</th>
-				<th style="width:70px">登入轉化率</th>
-				<th style="width:70px">第三方APK登入</th>
-				<th style="width:70px">總首次開啟數</th>
-				<th style="width:70px">ios首次開啟數</th>
-				<th style="width:70px">android首次開啟數</th>	
-				<th style="width:70px">第三方APK首次開啟數</th>
-				<th style="width:70px">首次開啟轉化率</th>
-				<th style="width:70px">首次開啟地區-台灣</th>
-				<th style="width:70px">首次開啟地區-香港</th>
-				<th style="width:70px">首次開啟地區-澳門</th>
-				<th style="width:70px">首次開啟地區-新加坡</th>
-				<th style="width:70px">首次開啟地區-馬來西亞</th>
-				<th style="width:70px">首次開啟地區-其他</th>
+				<th style="width:70px">遊戲</th>
+				<th style="width:70px">媒體</th>
+				<th style="width:70px">裝置</th>
+				<th style="width:70px">點擊</th>
+				<th style="width:70px">首次開啟</th>
+				<th style="width:70px">地區</th>
+				<th style="width:70px">額外設置-<br>登入(首次)</th>
+				<th style="width:70px">額外設置-<br>登入</th>
+				<th style="width:70px">額外設置-<br>登入(銷售)</th>
+				<th style="width:70px">額外設置-<br>儲值次數(首次)</th>
+				<th style="width:70px">額外設置-<br>儲值次數</th>
+				<th style="width:70px">額外設置-<br>儲值金額USD</th>
+				<th style="width:70px">額外設置-<br>創角(首次)</th>
+				<th style="width:70px">額外設置-<br>創角</th>
+				<th style="width:70px">額外設置-<br>創角(銷售)</th>
+				<th style="width:70px">額外設置-<br>升等(首次)</th>
+				<th style="width:70px">額外設置-<br>升等</th>
+				<th style="width:70px">額外設置-<br>升等(銷售)</th>		 	
 			</tr>
 		</thead>
 		<tbody>
 		<? $color = array('00aa00', '448800', '776600', 'aa4400', 'dd2200', 'ff0000');
-			foreach($query->result() as $row):
-				$startdate = strtotime($row->date);
-				$enddate = time();
-				$days = round(($enddate-$startdate)/3600/24) ; 
-				
-				switch($this->input->get("span")) {
-					case "weekly":
-						$year = substr($row->date, 0, 4);
-						$week = substr($row->date, 4, 2);
-						$show_date =  date("Y-m-d",strtotime($year."W".sprintf('%02d', $week))) 
-						      . "~" . date("m-d",strtotime($year."W".sprintf('%02d',$week)."7"));
-						break;
-					
-					case "monthly":
-						$show_date = $row->year . "-" . $row->date;
-						break;
-						
-					default:
-						$show_date = $row->date;
-						break;
-				}
-                
-                $download_count = $row->ios_download_count+$row->google_download_count+$row->apk_download_count;
-                $tw_download_count = $row->ios_tw_download_count+$row->google_tw_download_count+$row->apk_tw_download_count;
-                $hk_download_count = $row->ios_hk_download_count+$row->google_hk_download_count+$row->apk_hk_download_count;
-                $mo_download_count = $row->ios_mo_download_count+$row->google_mo_download_count+$row->apk_mo_download_count;
-                $sg_download_count = $row->ios_sg_download_count+$row->google_sg_download_count+$row->apk_sg_download_count;
-                $my_download_count = $row->ios_my_download_count+$row->google_my_download_count+$row->apk_my_download_count;
-		?>
+			foreach($query->result() as $row):?>
 			<tr>			
-				<td nowrap="nowrap"><?=$show_date?></td>
-				<td style="text-align:right"><?=number_format($row->new_login_count)?></td>
-				<td style="text-align:right"><?=number_format($row->login_count)?></td>
-				<td style="text-align:right"><?=number_format($row->device_count)?></td>
-				<td style="text-align:right"><?=number_format(($row->device_count)?$row->login_count/$row->device_count*100:0, 2)."%"?></td>
-				<td style="text-align:right"><?=number_format($row->apk_login_count)?></td>
-				<td style="text-align:right"><?=number_format($download_count)?></td>
-				<td style="text-align:right"><?=number_format($row->ios_download_count)?></td>
-				<td style="text-align:right"><?=number_format($row->google_download_count)?></td>
-				<td style="text-align:right"><?=number_format($row->apk_download_count)?></td>
-				<td style="text-align:right"><?=number_format(($row->login_count)?($download_count)/$row->login_count*100:0, 2)."%"?></td>
-				<td style="text-align:right"><?=number_format($tw_download_count)?></td>
-				<td style="text-align:right"><?=number_format($hk_download_count)?></td>
-				<td style="text-align:right"><?=number_format($mo_download_count)?></td>
-				<td style="text-align:right"><?=number_format($sg_download_count)?></td>
-				<td style="text-align:right"><?=number_format($my_download_count)?></td>
-				<td style="text-align:right"><?=number_format($download_count-$tw_download_count-$hk_download_count-$mo_download_count-$sg_download_count-$my_download_count)?></td>
+				<td nowrap="nowrap"><?=$row->date?></td>
+				<td nowrap="nowrap"><?=$row->game_id?></td>
+				<td nowrap="nowrap"><?=$row->media?></td>
+				<td nowrap="nowrap"><?=($row->platform=='google')?'android':$row->platform?></td>
+				<td style="text-align:right"><?=$row->click_count?></td>
+				<td style="text-align:right"><?=$row->install_count?></td>
+				<td nowrap="nowrap"><?=$row->country_code?></td>
+				<td style="text-align:right"><?=$row->af_login_unique?></td>	
+				<td style="text-align:right"><?=$row->af_login?></td>	
+				<td style="text-align:right"><?=$row->af_login_sales?></td>	
+				<td style="text-align:right"><?=$row->pay_unique_event_count?></td>	
+				<td style="text-align:right"><?=$row->pay_event_count?></td>	
+				<td style="text-align:right"><?=$row->pay_amount?></td>	
+				<td style="text-align:right"><?=$row->le_usercharactercreate_unique?></td>	
+				<td style="text-align:right"><?=$row->le_usercharactercreate?></td>	
+				<td style="text-align:right"><?=$row->le_usercharactercreate_sales?></td>	
+				<td style="text-align:right"><?=$row->le_usercharacterlevelup_unique?></td>	
+				<td style="text-align:right"><?=$row->le_usercharacterlevelup?></td>	
+				<td style="text-align:right"><?=$row->le_usercharacterlevelup_sales?></td>												
 			</tr>
 		<? endforeach;?>
 		</tbody>
