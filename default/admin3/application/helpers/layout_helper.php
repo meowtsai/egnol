698879487d2d37241b0c0c5a594f764d5aa8394d
root <root@test-payment.(none)>
2016-06-29 19:00:22 +0800

    Fix test_points message

--- default/admin3/application/helpers/layout_helper.php
+++ default/admin3/application/helpers/layout_helper.php
@@ -1,13 +1,13 @@
 <?php
 
-function output_result($msg=array())
+function output_result($msg=array(), $msg_type='error')
 {
-	if (empty($msg)) {
+	if (empty($msg) || $msg_type='success') {
 		echo '<div class="alert alert-success"><i class="icon-ok-circle"></i> 成功</div>';
 	}
 	else {
 		if ( ! is_array($msg)) $msg = array($msg);
-		echo '<div class="alert alert-error">';
+		echo '<div class="alert alert-'.$msg_type.'">';
 		foreach ($msg as $m) { 
 			echo '<i class="icon-remove-circle"></i> '.$m.'<br>';
 		}	
