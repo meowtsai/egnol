7b27fd2f7697ab1a60e730a933a60b63956862af
root <root@test-payment.(none)>
2016-10-24 18:45:49 +0800

    Add games/vxz

--- games/vxz/p/js/action.js
+++ games/vxz/p/js/action.js
@@ -102,6 +102,7 @@ $(function(){
 	$(window).load(function(){
 		$('.flexslider').flexslider({
 			animation: "slide",
+            controlNav: false,
 			start: function(slider){
 			$('body').removeClass('loading');
 			}
