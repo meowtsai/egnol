011b525701e37354a9b6c17f2197d702b5ad4e94
root <root@test-payment.(none)>
2016-11-08 16:52:17 +0800

    mobile optimization for service/list

--- default/application/views/service/listing.php
+++ default/application/views/service/listing.php
@@ -17,24 +17,24 @@
 		<ul class="server_case">
 			<? $no = $query->num_rows();
 				foreach($query->result() as $row):?>
-				<a href="<?=$longe_url?>service/view/<?=$row->id?>?site=<?=$site?>")">
+				<a href="<?=$longe_url?>service/view/<?=$row->id?>?site=<?=$site?>">
 					<li style="margin-top:10px;">
 						<table style="width:100%;">
 							<tr>
+								<div class="wrap_text" style="font-size: 11px;max-width:48%;min-width:48%;"><b>#<?=$row->id?></b></div>
+						        <div style="float:right; font-size: 9px; color: #A8A8A8; font-style: italic;overflow:visible;max-width:48%;min-width:48%;">
+							        <?=date('Y-m-d H:i', strtotime($row->create_time))?>
+						        </div>
+							</tr>
+							<tr>
 						        <td class="button">
-                                    <div class="wrap_text" style="max-width:12%;min-width:12%;"><b>#<?=$row->id?></b></div>
-								    <div class="wrap_text" style="max-width:55%;min-width:55%;"><?=$row->content?></div>
+									<div class="wrap_text" style="max-width:72%;min-width:72%;"><span style="color:#e6290f"><?=$row->content?></span></div>
 								    <div class="wrap_text" style="max-width:3%;min-width:3%;">|</div>
-								    <div class="wrap_text" style="float:right; max-width:23%; width:23%;">
+								    <div class="wrap_text" style="float:right; max-width:18%; width:18%;">
 									    <?=$question_status[$row->status]?>
                                         <? if ($row->status=='2' && $row->is_read=='0') echo '<span class="field" style="color:red">(未讀)</span>'?>
 								    </div>
 							    </td>
-							</tr>
-							<tr>
-						        <td style="float:right; font-size: 8px; color: #D8D8D8; font-style: italic;overflow:visible;">
-							        <?=date('Y-m-d H:i', strtotime($row->create_time))?>
-						        </td>
 							</tr>
 						</table>
 					</li>
