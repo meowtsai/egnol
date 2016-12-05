31240e38d0217d3966438cfd70e2abbcf3cf9990
root <root@test-payment.(none)>
2016-12-05 17:36:53 +0800

    admin3 vip fix

--- longedb.sql
+++ longedb.sql
@@ -945,6 +945,7 @@ CREATE TABLE `vip_tickets` (
   `billing_name` varchar(20) DEFAULT NULL,
   `auth_admin_uid` int(11) DEFAULT NULL,
   `auth_time` timestamp NULL DEFAULT NULL,
+  `product_id` varchar(60) DEFAULT NULL,
   `note` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='VIP訂單';
