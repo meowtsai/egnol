d4167fd4f1ef2ce1bd4120065b01887503ec5bd1
root <root@test-payment.(none)>
2017-01-11 13:40:13 +0800

    admin3 service/statistics

--- default/admin3/application/controllers/service.php
+++ default/admin3/application/controllers/service.php
@@ -442,6 +442,95 @@ class Service extends MY_Controller {
 			->render();
 	}	
 	
+		
+	function statistics()
+	{			
+		$this->_init_service_layout();
+		
+		if ($this->input->get("action")) 
+		{			
+			header("Cache-Control: private");
+
+			$this->DB2->start_cache();
+			
+			$this->input->get("question_id") && $this->DB2->where("q.id", $this->input->get("question_id"));
+			$this->input->get("uid") && $this->DB2->where("q.uid", $this->input->get("uid"));
+			$this->input->get("status")<>'' && $this->DB2->where("q.status", $this->input->get("status"));
+			$this->input->get("type") && $this->DB2->where("q.type", $this->input->get("type"));
+			$this->input->get("game") && $this->DB2->where("gi.game_id", $this->input->get("game"));
+			$this->input->get("character_name") && $this->DB2->where("q.character_name", $this->input->get("character_name"));			
+			$this->input->get("email") && $this->DB2->where("u.email", $this->input->get("email"));
+			$this->input->get("content") && $this->DB2->like("q.content", $this->input->get("content"));
+			
+			$this->input->get("allocate_auid") && $this->DB2->where("q.allocate_admin_uid", $this->input->get("allocate_auid"));
+			$this->input->get("allocate_status") && $this->DB2->where("q.allocate_status", $this->input->get("allocate_status"));
+					
+			$this->input->get("todo") && $this->DB2->where("(q.status=1 or q.allocate_status=2 and q.status<>4)", null, false);
+			
+			$this->DB2
+				->select("COUNT(*) as cnt, au.name as admin_uname")
+				->from("question_replies qr")
+				->join("questions q", "q.id=qr.question_id", "left")
+				->join("servers gi", "gi.server_id=q.server_id", "left")
+				->join("users u", "u.uid=q.uid", "left")
+				->join("admin_users au", "au.uid=q.admin_uid", "left")
+                ->where("qr.is_official >", 0);
+						
+			if ($this->input->get("account")) {
+				$this->DB2->join("users u", "u.uid=q.uid", "left")
+					->where("u.email", $this->input->get("account"))
+					->or_where("u.mobile", $this->input->get("account"));
+			}
+            
+			if ($this->input->get("replies") || $this->input->get("cs_admin")) {
+                if ($this->input->get("replies")) $this->DB2->like("qr.content", $this->input->get("replies"), 'both');
+                if ($this->input->get("cs_admin")) $this->DB2->where("qr.admin_uid", $this->input->get("cs_admin"));
+			}
+									
+			if ($this->input->get("start_date")) {
+				$start_date = $this->DB2->escape($this->input->get("start_date"));
+				if ($this->input->get("end_date")) {
+					$end_date = $this->DB2->escape($this->input->get("end_date").":59");
+					$this->DB2->where("q.create_time between {$start_date} and {$end_date}", null, false);	
+				}	
+				else $this->DB2->where("q.create_time >= {$start_date}", null, false);
+			}
+		
+			switch ($this->input->get("action"))
+			{										
+				case "查詢":					
+					$this->DB2->stop_cache();
+					
+					$query = $this->DB2->group_by("au.name")->get();					
+                    
+					break;
+			}
+						
+			$this->DB2->stop_cache();
+			$this->DB2->flush_cache();
+                  
+		}
+		else {
+			$default_value = array(
+				'use_default' => true,
+				'start_date' => date('Y-m-d')." 00:00",
+			);
+			$_GET = $default_value;			
+		}
+					
+		$games = $this->DB2->from("games")->get();
+		$cs_admins = $this->DB2->from("admin_users")->where_in("role", array("cs", "cs_master"))->get();
+		
+		$this->g_layout
+			->add_breadcrumb("查詢")	
+			->set("query", isset($query) ? $query : false)
+			->set("games", $games)
+			->set("cs_admins", $cs_admins)
+			->add_js_include("service/get_list")
+			->add_js_include("jquery-ui-timepicker-addon")
+			->render();
+	}
+	
 	function edit_reply($id)
 	{
 		$row = $this->DB2->where("id", $id)->from("question_replies")->get()->row();
