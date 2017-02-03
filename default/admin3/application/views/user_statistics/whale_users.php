a5d2960eda088fe2cdb0d56bce1f270373c3d996
Meow <Meow>
2017-02-03 11:24:24 +0800

    whale users addtional info added

--- default/admin3/application/views/user_statistics/whale_users.php
+++ default/admin3/application/views/user_statistics/whale_users.php
@@ -47,7 +47,7 @@
 		<thead>
 			<tr>
 				<th nowrap="nowrap" rowspan="2">排名</th>
-				<th style="width:70px" rowspan="2">帳號</th>
+				<th style="width:80px" rowspan="2">帳號</th>
 				<th style="width:160px" rowspan="2">角色</th>
                 <th style="width:70px" rowspan="2">原廠ID</th>
 				<th style="width:110px" rowspan="2">所在伺服器</th>
@@ -67,8 +67,6 @@
 				<td nowrap="nowrap"><?=$seq++?></td>
 				<td style="text-align:right">
                     <a href="<?=site_url("member/view/{$row->uid}")?>"><?=$row->uid?>  </a>
-            
-                    <?=$row->character_name?> 
                     <? if ($row->days_inserted==0):?>
                     <span class="label label-important">New</span>
                     <? endif;?>
