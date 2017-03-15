bef2eaf4336d851530200deff687f98ae05a9a40
root <root@test-payment.(none)>
2017-03-15 17:55:26 +0800

    Add service links to cs page

--- default/application/views/service/index.php
+++ default/application/views/service/index.php
@@ -4,6 +4,14 @@
 			<a href="<?=$game_url?>" title="首頁" rel="v:url" property="v:title">首頁</a> > <a href="<?=$longe_url?>service?site=<?=$site?>" title="客服中心" rel="v:url" property="v:title">客服中心</a>
 		</div>
 		<div class="login-button">
+			
+			<p>
+				<span style="vertical-align:text-bottom;">遊戲進行以外的問題可先參考</span>
+				<a href="<?=$longe_url?>member/service_agreement?site=<?=$site?>" style="vertical-align:text-bottom;color:#a50000">會員服務條款</a><span style="vertical-align:text-bottom;">、</span>
+				<a href="<?=$longe_url?>member/member_agreement?site=<?=$site?>" style="vertical-align:text-bottom;color:#a50000">個資同意書</a><span style="vertical-align:text-bottom;">與</span>
+				<a href="<?=$longe_url?>member/privacy_agreement?site=<?=$site?>"  style="vertical-align:text-bottom;color:#a50000">隱私權政策</a>
+			</p>
+			<br>
 			<p>
 				<img style="cursor:pointer;" src="<?=$longe_url?>p/image/server/server-btn.png" class="button_submit" onclick="javascript:location.href='<?=$longe_url?>service/question?site=<?=$site?>'" />&nbsp;
 				<img style="cursor:pointer;" src="<?=$longe_url?>p/image/server/server-btn2.png" class="button_submit" onclick="javascript:location.href='<?=$longe_url?>service/listing?site=<?=$site?>'" />
