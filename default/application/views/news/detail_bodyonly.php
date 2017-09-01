f1fbef4855e3668dd8ccce9098e4377ab51934fc
root <root@test-payment.(none)>
2017-09-01 15:32:12 +0800

    Remove date from news/detail_bodyonly

--- default/application/views/news/detail_bodyonly.php
+++ default/application/views/news/detail_bodyonly.php
@@ -8,7 +8,6 @@
                     <div class="bd" >
                         <div style="font-size:18px; color:#222; font-weight:bold;">
                             <?=$row->title?>
-                            <span style="font-size:13px; color:#191;"><?=date("Y-m-d", strtotime($row->start_time))?></span>
                         </div>
                         <div class="fixck">
                             <?=$row->content?>
