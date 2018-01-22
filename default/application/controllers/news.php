7ffe7a0be19ec6f09387f5caa8336be510f83873
meowtsai <shihfan.tsai@gmail.com>
2018-01-22 16:51:05 +0800

    add two functions for ssydj news

--- default/application/controllers/news.php
+++ default/application/controllers/news.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class News extends MY_Controller {
-	
+
 	function __construct()
 	{
 		parent::__construct();
@@ -11,16 +11,16 @@ class News extends MY_Controller {
 	{
 		$site = $this->_get_site();
 		$type = $this->input->get_post("type") ? $this->input->get_post("type") : 0;
-		
-		$this->load->model("g_bulletins");		
+
+		$this->load->model("g_bulletins");
 		$this->load->library('pagination');
-		
+
 		$this->pagination->initialize(array(
 				'base_url'	=> site_url("news?site={$site}&type={$type}"),
 				'total_rows'=> $this->g_bulletins->get_count($site, $type),
 				'per_page'	=> 7
-		));	
-		
+		));
+
         $this->_init_layout()
             ->add_css_link("login")
             ->add_css_link("news")
@@ -28,11 +28,11 @@ class News extends MY_Controller {
             ->set("news", $this->g_bulletins->get_list($site, $type, 7, $this->input->get("record")))
             ->standard_view();
 	}
-    
+
 	function preview($id)
 	{
 		$site = $this->_get_site();
-        
+
 		$this->load->model("g_bulletins");
 		$row = $this->g_bulletins->get_preview($site, $id);
 		if ($row == false) {die("無此記錄");}
@@ -41,19 +41,19 @@ class News extends MY_Controller {
 			->add_css_link("login")
 			->add_css_link("news")
 			->set("row", $row)
-			->standard_view("news/detail");		
+			->standard_view("news/detail");
 	}
-	
+
 	function detail($id)
 	{
 		$site = $this->_get_site();
-        
+
 		$bodyonly = $this->input->get("bodyonly");
-        
+
 		$this->load->model("g_bulletins");
 		$row = $this->g_bulletins->get_bulletin($site, $id);
 		if ($row == false || $row->priority == 0) {die("無此記錄");}
-		
+
         if ($bodyonly) {
             $this->_init_layout()
                 ->add_css_link("login")
@@ -67,5 +67,37 @@ class News extends MY_Controller {
                 ->set("row", $row)
                 ->standard_view();
         }
+	}
+
+	function get_news_list($site)
+	{
+		//公告=1 活動=2 系統=3
+		$query = $this->db->query("SELECT c1.id, c1.title, c1.game_id,c1.type,c1.start_time, c1.is_active
+			FROM bulletins c1
+			LEFT OUTER JOIN bulletins c2
+			  ON (c1.type  = c2.type  AND c1.start_time < c2.start_time)
+			where c1.game_id ='{$site}' and c2.game_id='{$site}' and c1.type <>'99' AND c1.start_time < now()
+			GROUP BY c1.id
+			HAVING COUNT(*) < 5;");
+
+		$data = array();
+		foreach($query->result() as $row) {
+			$data[] = array(
+				'id' => $row->id,
+				'title' => $row->title,
+				'date' =>  date("Y/m/d", strtotime($row->create_time)),
+				'category' => ($row->type == 1? "news": ($row->type == 2?"event":"notice") ),
+			);
+		}
+
+
+		die(json_encode($data));
+	}
+
+	function get_news_content($news_id)
+	{
+		//公告=1 活動=2 系統=3
+		$row = $this->db->from("bulletins")->where("id", $news_id)->get()->row();
+		die(json_encode($row));
 	}
 }
