b21da5abf8450b256f2af6881cd04f80a4d3a9b4
meowtsai <shihfan.tsai@gmail.com>
2018-07-13 11:10:14 +0800

    add agreement link to service

--- default/application/views/service_quick/index.php
+++ default/application/views/service_quick/index.php
@@ -31,14 +31,6 @@
 			</p>
 
 		</div>
-		<?
-		$user_ip = $_SERVER['REMOTE_ADDR'];
-		if ($user_ip=="61.220.44.200"):?>
-		<div class="alert" style="text-align:center;padding:5px;">＊ 發問前也可以參考我們的<a href="<?=$longe_url?>game_faq?site=<?=$site?>"><b>[常見問題]</b></a>喔!</div>
-		<?endif;?>
-
-
-
         <? if ($is_ingame):?>
             <div class="notes" style="text-align:center;padding:5px;">提醒您：需附檔案回報時，請直接利用官網線上提問，謝謝。</div>
         <? endif;?>
