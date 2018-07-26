c278f036d61f8aa6e9f62462c365ba7fb1c30d34
meowtsai <shihfan.tsai@gmail.com>
2018-07-26 15:56:36 +0800

    add faq questions to report form

--- default/application/views/service_quick/question.php
+++ default/application/views/service_quick/question.php
@@ -99,7 +99,9 @@
 					<?
 					//echo $_SERVER['HTTP_USER_AGENT'];
 					$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
-					if (!$is_ingame && (stripos($ua,'android')== false) ):?>
+					//$is_ingame
+					if (!$is_ingame ):
+					?>
 					<tr>
 						<th>圖片附件</th><td style="white-space:pre-wrap;"><img src="<?=$longe_url?>p/image/server/server-pic-btn1.png" class="pic_btn"> <input type="file" name="file01" class="pic_input" /></td>
 					</tr>
@@ -125,7 +127,7 @@
           <? else:?>
 					<tr>
 						<td style="white-space:pre-wrap;" colspan="2">
-            <div class="notes" style="text-align:center;padding:5px;">提醒您：安卓機型需附檔案回報時，請直接利用官網線上提問，謝謝。</div></td>
+            <div class="notes" style="text-align:center;padding:5px;">提醒您：需附檔案回報時，請直接利用官網線上提問，謝謝。</div></td>
                     </tr>
                     <? endif;?>
 				</table>
