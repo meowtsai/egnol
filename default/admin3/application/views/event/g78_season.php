cff60dce2000aeb4e640685071249d5f50fdd8ed
meowtsai <shihfan.tsai@gmail.com>
2018-10-16 17:43:09 +0800

    g78 s2 season result query

--- default/admin3/application/views/event/g78_season.php
+++ default/admin3/application/views/event/g78_season.php
@@ -6,6 +6,7 @@
   <input type="text" name="player_id" value="<?=$this->input->get("player_id")?>" class="input-medium required" placeholder="玩家id">
 	<button type="submit" class="btn"><i class="icon-search"></i> 查詢</button>
 
+<div>獲獎條件為 60 場戰鬥與處罰比例小於 0.025 </div>
 </form>
 <? if ($query):?>
 <? if ($query->num_rows() == 0):?>
@@ -19,7 +20,7 @@
 </div>
 
 
-<table class="table table-striped" style="width:300px">
+<table class="table table-striped" style="width:600px">
 	<thead>
 		<tr>
             <th style="width:60px;text-align:center">player_id</th>
@@ -27,6 +28,7 @@
             <th style="width:20px;text-align:center">punish_cnt</th>
             <th style="width:30px;text-align:center">punish_ag</th>
             <th style="width:20px;text-align:center">tag</th>
+            <th style="width:50px;text-align:center">結果</th>
         </tr>
 	</thead>
 	<tbody>
@@ -37,6 +39,14 @@
 			<td style="text-align:center;"><?=$row->punish_cnt?></td>
 			<td style="text-align:center;"><?=$row->punish_ag?></td>
       <td style="text-align:center;"><?=$row->tag?></td>
+
+      <td style="text-align:center;">
+        <?if($row->season_5v5cnt >= 60 && $row->punish_ag < 0.025):?>
+            獲獎
+        <?else:?>
+            未獲獎
+        <?endif;?>
+      </td>
 		</tr>
 	<? endforeach;?>
 	</tbody>
